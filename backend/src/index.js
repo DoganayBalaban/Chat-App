7661d125cfc79781ce7adaa56290c7d6f24c4735
doganaybalaban <dbalaban1907@gmail.com>
2024-11-28 20:00:17 +0300

    added messages api routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,12 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
