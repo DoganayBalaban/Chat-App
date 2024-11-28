39ead8e027e000a36433e61dbe248b94496a27c4
doganaybalaban <dbalaban1907@gmail.com>
2024-11-28 19:30:09 +0300

    added updateProfile, logout and checkAuth api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
@@ -74,5 +75,43 @@ export const login = async (req, res) => {
   }
 };
 export const logout = (req, res) => {
-  res.send("Hello from logout route");
+  try {
+    res.cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+    res.status(200).json({ message: "Logged out" });
+  } catch (error) {
+    console.error("Error in logout controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+export const updateProfile = async (req, res) => {
+  const { profilePic } = req.body;
+  const userId = req.user._id;
+  try {
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile picture is required" });
+    }
+    const uploadRes = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadRes.secure_url,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error("Error in updateProfile controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+export const checkAuth = async (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.error("Error in checkAuth controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 import authRoutes from "./routes/auth.route.js";
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
